bc9ab5fb3785f1bb7863a39e5c3fea34869fa6d7
Joseph Catrambone <jo.jcat@gmail.com>
2020-05-10 15:46:07 -0700

    First import.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clipboard::{ClipboardProvider, ClipboardContext};
-use minifb::{Key, Window, WindowOptions};
+use minifb::{Key, Window, WindowOptions, Scale};
 use qrcode::QrCode;
 use std::thread;
 
@@ -16,30 +16,55 @@ fn main() {
 	let code = QrCode::new(data.as_bytes()).unwrap();
 	//image = code.render::<Luma<u8>>().build()
 	//image.save()
-	//let s = code.render::<char>().quiet_zone(false).module_dimensions(2, 1).build();
-	//println!("{}", s);
-	let mut width = 0;
-	let mut height = 0;
-	let framebuffer: Vec<u32> = code.render::<char>().quiet_zone(true).module_dimensions(1, 1).light_color('1').dark_color('0').build().chars().map(|character|{
-		match character {
-			'1' => { width += 1; 0xFFFFFFFF as u32 },
-			'0' => { width += 1; 0x0 as u32 },
-			'\n' => { width += 1; height += 1; 0x0 as u32 }
-			_ => { width += 1; 0x0 as u32 }
-		}
-	}).collect();
+	//let qr_string = code.render::<char>().quiet_zone(false).module_dimensions(1, 1).build();
+	let qr_string = code.render::<char>().quiet_zone(true).module_dimensions(1, 1).light_color('1').dark_color('0').build();
+	let qr_strings:Vec<&str> = qr_string.split_whitespace().collect();
+	//println!("{}", qr_string);
 
-	println!("Width / Height: {} {}", width, height);
+	// HACK: Reset width and height to zero so that we populate the window on first call.
+	let mut width = WIDTH;
+	let mut height = HEIGHT;
+	let mut framebuffer: Vec<u32> = Vec::<u32>::with_capacity(width*height);
+	width = 0;
+	height = 0;
 
 	// Pop up a window and display the image until someone hits escape.
 	let mut window_options = WindowOptions::default();
 	window_options.borderless = true;
 	window_options.title = false;
-	window_options.resize = true;
-	let mut window = Window::new("", width, height, window_options).unwrap_or_else(|e| {
+	window_options.resize = false; // TODO: Figure out why the resizing glitch happens so we can make this resizable.
+	window_options.scale = Scale::X1;
+	let mut window = Window::new("", WIDTH, HEIGHT, window_options).unwrap_or_else(|e| {
 		panic!("{}", e);
 	});
 	while window.is_open() && !window.is_key_down(Key::Escape) {
+		{
+			let new_size = window.get_size();
+			if new_size.0 != width || new_size.1 != height {
+				let pixel_height = new_size.1 / qr_strings.len();
+				let pixel_width = new_size.0 / qr_strings[0].len();
+				//println!("New size: {} {}  Old size: {} {}.", new_size.0, new_size.1, width, height);
+				//println!("Pixel width / height: {} {}", pixel_width, pixel_height);
+				width = new_size.0;
+				height = new_size.1;
+				framebuffer.resize(width*height, 0);
+				for (y, row_data) in qr_strings.iter().enumerate() {
+					for (x, column_data) in row_data.chars().into_iter().enumerate() {
+						for dy in 0..=pixel_height {
+							for dx in 0..=pixel_width {
+								framebuffer[(x*pixel_width + dx)+(y*pixel_height + dy)*width] = 
+									if column_data == '1' {
+										0xFFFFFFFFu32
+									} else {
+										0x0
+									}
+							}
+						}
+					}
+				}
+			}
+		}
+
 		window.update_with_buffer(&framebuffer).unwrap();
 		thread::sleep_ms(1); // yield_now() uses 100% cpu.  Odd.
 	}
